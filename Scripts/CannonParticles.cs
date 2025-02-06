5646d80bf36bbb5165f1dc44bd1a58a4dd8bee51
Szymon Kaczmarek <szymon.kaczmarek111@gmail.com>
2025-02-06 11:27:39 +0100

    Add sound

--- Scripts/CannonParticles.cs
+++ Scripts/CannonParticles.cs
@@ -11,11 +11,16 @@ public partial class CannonParticles : Node3D
     private GpuParticles3D smoke;
 	[Export]
 	private AudioStreamPlayer3D sound;
+    [Export]
+    private Vector2 pitchScaleRange;
+
+    private RandomNumberGenerator rng = new RandomNumberGenerator();
 
     public void Fire()
 	{
 		debris.Emitting = true;
 		fire.Emitting = true;
+        sound.PitchScale = rng.RandfRange(pitchScaleRange.X, pitchScaleRange.Y);
 		sound.Play();
         GetTree().CreateTimer(0.1).Timeout += () => smoke.Emitting = true;
         GetTree().CreateTimer(2.5).Timeout += OnTimeout;
