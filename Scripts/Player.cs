f18ef11005d41d1edc40d8d21b79594e7a6879b7
Szymon Kaczmarek <szymon.kaczmarek111@gmail.com>
2025-02-06 11:53:08 +0100

    Add autofire

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -4,27 +4,63 @@ using System.Collections.Generic;
 
 public partial class Player : Node3D
 {
+    [Export]
+    private Vector2 sensitivity;
+    [Export]
+    private Vector2 angleRange;
 	[Export]
 	private Cannon[] cannons;
+
+    private bool mouseButtonDown = false;
+    private double timer = 0;
+    private int currentCannonIndex;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+        Input.MouseMode = Input.MouseModeEnum.Captured;
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+    public override void _Process(double delta)
+    {
+        if (timer > 0)
+        {
+            timer -= delta;
+        }
+
+        if (mouseButtonDown && timer <= 0)
+        {
+            cannons[currentCannonIndex].Fire();
+            currentCannonIndex = Mathf.PosMod(currentCannonIndex + 1, cannons.Length);
+            timer = 1 / PlayerStats.Instance.fireRate;
+        }
+    }
 
     public override void _Input(InputEvent @event)
     {
-        if (@event is InputEventMouseButton mouseButton)
-		{
-			if (mouseButton.ButtonIndex == MouseButton.Left && mouseButton.Pressed)
-			{
-				cannons[0].Fire();
-			}
-		}
+        switch (@event)
+        {
+            case InputEventMouseMotion mouseMotion:
+                Vector3 rotation = RotationDegrees;
+                rotation += new Vector3(mouseMotion.Relative.Y * sensitivity.Y, -mouseMotion.Relative.X * sensitivity.X, 0);
+                rotation.X = Mathf.Clamp(rotation.X, -angleRange.X, angleRange.X);
+                rotation.Y = Mathf.Clamp(rotation.Y, -angleRange.Y, angleRange.Y);
+                RotationDegrees = rotation;
+                break;
+
+            case InputEventMouseButton mouseButton:
+                if (mouseButton.ButtonIndex == MouseButton.Left)
+                {
+                    mouseButtonDown = mouseButton.Pressed;
+                }
+                break;
+
+            case InputEventKey key:
+                if (key.Pressed && key.Keycode == Key.Escape)
+                {
+                    GetTree().Quit();
+                }
+                break;
+        }
     }
 }
--- Scripts/PlayerStats.cs
+++ Scripts/PlayerStats.cs
@@ -3,6 +3,13 @@ using System;
 
 public partial class PlayerStats : Node3D
 {
+    public static PlayerStats Instance { get; private set; }
+
     [Export]
-    public float fireRate;
+    public double fireRate;
+
+    public override void _Ready()
+    {
+        Instance = this;
+    }
 }
