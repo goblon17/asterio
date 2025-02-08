c9dd2d388cfaa6b88f1e7fd57aba64531b2029ed
Szymon Kaczmarek <szymon.kaczmarek111@gmail.com>
2025-02-08 14:39:55 +0100

    Add game over animation

--- Scripts/GameOverManager.cs
+++ Scripts/GameOverManager.cs
@@ -9,6 +9,22 @@ public partial class GameOverManager : Node3D
 	private Button mainMenuButton;
 	[Export]
 	private Button quitButton;
+    [Export]
+    private double timeToExplosion;
+    [Export]
+    private Vector2 rotationSpeedRange;
+    [Export]
+    private CannonParticles explosionParticles;
+    [Export]
+    private Node3D ship;
+    [Export]
+    private Node3D asteroid;
+    [Export]
+    private Node3D asteroidEndPos;
+
+    private RandomNumberGenerator rng = new RandomNumberGenerator();
+
+    private Vector3 rotationSpeed;
 
 	public override void _Ready()
 	{
@@ -16,7 +32,25 @@ public partial class GameOverManager : Node3D
         score.Text = score.Text.Replace("{num}", GameValues.Instance.score.ToString());
         mainMenuButton.Pressed += OnMainMenuPressed;
         quitButton.Pressed += OnQuitPressed;
-	}
+        float x = rng.RandfRange(rotationSpeedRange.X, rotationSpeedRange.Y);
+        float y = rng.RandfRange(rotationSpeedRange.X, rotationSpeedRange.Y);
+        float z = rng.RandfRange(rotationSpeedRange.X, rotationSpeedRange.Y);
+        rotationSpeed = new Vector3(x, y, z);
+
+        Tween tween = asteroid.CreateTween().SetParallel();
+        tween.TweenProperty(asteroid, Node3D.PropertyName.GlobalPosition.ToString(), asteroidEndPos.GlobalPosition, timeToExplosion);
+        tween.TweenMethod(Callable.From((double time) =>
+        {
+            asteroid.RotationDegrees = rotationSpeed * (float)time;
+        }), 0, timeToExplosion, timeToExplosion);
+        tween.SetParallel(false);
+        tween.TweenCallback(Callable.From(() =>
+        {
+            explosionParticles.Fire();
+            asteroid.QueueFree();
+            ship.QueueFree();
+        }));
+    }
 
     private void OnQuitPressed()
     {
